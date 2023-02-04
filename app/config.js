311de8228571ccde0a57a64527c998072d04af1e
ooooo <un4v5s@gmail.com>
2023-02-05 05:18:50 +0900

    edit code formatting

--- app/config.js
+++ app/config.js
@@ -1,10 +1,10 @@
 require(`dotenv`).config();
-const {envInt} = require(`./getEnvVariable`);
+const { envInt } = require(`./getEnvVariable`);
 
 const config = {
-    version: require(`../package`).version,
-    port: () => envInt(`PORT`, 7776),
-    waitBeforeServerClose: envInt(`WAIT_BEFORE_SERVER_CLOSE`, 0) * 1000,
+  version: require(`../package`).version,
+  port: () => envInt(`PORT`, 8080),
+  waitBeforeServerClose: envInt(`WAIT_BEFORE_SERVER_CLOSE`, 0) * 1000,
 };
 
 module.exports = config;
--- app/server.js
+++ app/server.js
@@ -2,12 +2,13 @@ const app = require(`./app`);
 const config = require(`./config`);
 
 module.exports = {
-    startServer({shutdownHandler}) {
-        const server = app().listen(config.port(), (err) => {
-            if (err) {
-                return console.log(err.message);
-            }
-        });
-        shutdownHandler.setServer(server);
-    },
+  startServer({ shutdownHandler }) {
+    const server = app().listen(config.port(), (err) => {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log(`server started: http://localhost:%s`,config.port());
+    });
+    shutdownHandler.setServer(server);
+  },
 };
